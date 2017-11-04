381b5ea423921b45c682b074e5ffb0c57db639cb
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2017-11-04 20:00:39 +0100

    Add Laravel Json API from CloudCreativity Package

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,15 @@
 
 namespace App\Exceptions;
 
+use CloudCreativity\LaravelJsonApi\Exceptions\HandlesErrors;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Neomerx\JsonApi\Exceptions\JsonApiException;
 
 class Handler extends ExceptionHandler
 {
+    use HandlesErrors;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -14,6 +18,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         //
+        JsonApiException::class,
     ];
 
     /**
@@ -48,6 +53,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($this->isJsonApi()) {
+            return $this->renderJsonApi($request, $exception);
+        }
+
         return parent::render($request, $exception);
     }
 }
